b1d5b2777729b6ec91f8f640bcd167139f523bbc
Goga Imerlishvili <gogaimerlishvili1@gmail.com>
2025-03-11 14:32:52 +0400

    update

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -8,7 +8,7 @@ const Sidebar = () => {
             {/* TOP LOGO */}
         <div className='flex gap-3 justify-between md:justify-normal items-center pt-8'>
         <div>logo</div>
-        <h1 className='font-extrabold text-2xl'>EDSTOCK</h1>
+        <h1 className='font-extrabold text-2xl'>Giorgi</h1>
     
         <button className='md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' onClick={() => {}} >
             <Menu  className='w-4 h-4 bg-red-700'/>
